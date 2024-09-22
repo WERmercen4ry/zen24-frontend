e184a20c07e8bbafe4a3796736f31f0ae7072ca3
VangNV <vangnguyen28122001@gmail.com>
2024-09-22 20:43:24 +0700

    create BookingScreen logic

--- src/layouts/user/UserFullLayout.js
+++ src/layouts/user/UserFullLayout.js
@@ -1,15 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
+import Loader from "../loader/Loader";
 import { Container } from "reactstrap";
-
+import { useContext } from "react";
+import { LoaderContext } from "../loader/LoaderContext"; 
 const FullLayout = () => {
+  const { loading } = useContext(LoaderContext);
   return (
     <main>
       <div className="pageWrapper d-lg-flex">
         <div className="contentArea">
           {/********header**********/}
           <Header />
+          {loading && <Loader />}
           {/********Middle Content**********/}
           <Container className="wrapper-user " fluid>
             <Outlet />
