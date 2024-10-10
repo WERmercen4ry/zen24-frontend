5101eec27b1b7c25bec103f946cb75e5cff6d0f5
VangNV <vangnguyen28122001@gmail.com>
2024-10-10 19:47:00 +0700

    fix notification icon

--- src/layouts/user/Header.js
+++ src/layouts/user/Header.js
@@ -78,16 +78,13 @@ const Header = () => {
           {/* Dropdown cho chuông thông báo */}
           <Dropdown isOpen={dropdownOpen} toggle={toggleDropdown}>
             <DropdownToggle className="inbox-btn" tag="button">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
+              <i
                 width="16"
                 height="16"
-                fill="currentColor"
-                className="bi bi-bell-fill"
-                viewBox="0 0 16 16"
+                className="bi bi-bell-fill" color="primary"
               >
-                <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901" />
-              </svg>
+    
+              </i>
               <span className="msg-count">
                 {" "}
                 {notifications.filter((notification) => !notification.is_read)
