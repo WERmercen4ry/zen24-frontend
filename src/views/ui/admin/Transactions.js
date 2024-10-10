c38282b2c197cbca2acc5eeaf74a27247179e26b
Tient <tienthai0428@gmail.com>
2024-10-10 15:29:43 +0700

    Thời Khóa Biểu record

--- src/views/ui/admin/Transactions.js
+++ src/views/ui/admin/Transactions.js
@@ -134,9 +134,9 @@ const Transactions = () => {
             value={limit}
             onChange={handleLimitChange}
           >
-            <option value={5}>5</option>
-            <option value={10}>20</option>
-            <option value={20}>50</option>
+            <option value={10}>10</option>
+            <option value={20}>20</option>
+            <option value={50}>50</option>
             <option value={100}>100</option>
           </select>
         </div>
